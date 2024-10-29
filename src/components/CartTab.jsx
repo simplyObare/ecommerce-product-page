58800858b8589e50568a831602a05007c9cc6d30
Josephat Obare <josephatowiny@gmail.com>
2024-10-29 18:29:52 +0300

    Update CartTab.jsx

--- src/components/CartTab.jsx
+++ src/components/CartTab.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 const CartTab = () => {
   const cart = useSelector((state) => state.cart)
   return (
-    <div className=" max-w-[350px] sm:w-[350px] md:w-[350px] min-w-[315px] h-[250px] bg-white sm:shadow-2xl md:shadow-2xl lg:shadow-2xl shadow-none sm:shadow-orange-default/45 md:shadow-orange-default/45 lg:shadow-orange-default/45 sm:border-white md:border-white lg:border-white rounded-xl absolute sm:top-16 md:top-16 lg:top-16 top-[100px] sm:-right-24 md:-right-24 lg:-right-24 right-1">
+    <div className=" max-w-[350px] sm:w-[350px] md:w-[350px] min-w-[315px] h-[250px] bg-white sm:shadow-2xl md:shadow-2xl lg:shadow-2xl shadow-none sm:shadow-orange-default/45 md:shadow-orange-default/45 lg:shadow-orange-default/45 sm:border-white md:border-white lg:border-white rounded-xl absolute sm:top-16 md:top-16 lg:top-16 top-[100px] sm:-right-28 md:-right-28 lg:-right-28 right-1">
       <h1 className="text-blue-dark font-bold p-6  border-b border-orange-pale  shadow-sm shadow-orange-pale">
         Cart
       </h1>
